03beedcd29b9c0f087677e603d5838d3f858c1d8
Cristhian Santos <cristhianfelipe0@gmail.com>
2022-03-14 18:44:18 -0300

    ✨ feat: installation of dependency packages and configuration of main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running in: http://localhost:${port}/swagger/`);
 }
 bootstrap();
