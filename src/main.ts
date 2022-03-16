51d629372916f315c44cd7afd33f38ffb917e72c
Cristhian Santos <cristhianfelipe0@gmail.com>
2022-03-15 21:51:37 -0300

    ✨ feat: add enable cors in main

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { BEARER_AUTH_NAME } from './shared/const';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
