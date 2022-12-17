daf0b1bea12f8d294c487c62fdfdfc07c815ddaa
Cristhian Santos <70783719+CristhianFSantos@users.noreply.github.com>
2022-12-17 12:33:33 -0300

    📦 refactor: change email settings and remove fixed credentials

--- src/mail/mailer.config.ts
+++ src/mail/mailer.config.ts
@@ -2,22 +2,15 @@ import { MailerOptions } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import * as path from 'path';
 
+const pathTemplate = '../../src/mail/templates';
 export const mailerConfig: MailerOptions = {
-  // remetente
-  transport: `smtps://nestjs.mail.api@gmail.com:12345678Qq!@smtp.gmail.com`,
+  transport: process.env.EMAIL_CREDENTIALS_SMTP.toString(),
   template: {
-    dir: path.resolve(__dirname, '..', '..', 'src', 'mail', 'templates'),
+    dir: path.resolve(__dirname, pathTemplate),
     adapter: new HandlebarsAdapter(),
     options: {
       extName: '.hbs',
-      layoutsDir: path.resolve(
-        __dirname,
-        '..',
-        '..',
-        'src',
-        'mail',
-        'templates',
-      ),
+      layoutsDir: path.resolve(__dirname, pathTemplate),
     },
   },
 };
